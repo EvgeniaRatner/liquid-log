f29710304a11547e1beda06c674f71043eabf8cd
RatnerEvgenia <eu.ratner@yandex.ru>
2017-12-19 22:40:07 +0500

    +parsers

--- src/main/java/ru/naumen/sd40/log/parser/Parsers/GCParser.java
+++ src/main/java/ru/naumen/sd40/log/parser/Parsers/GCParser.java
@@ -1,4 +1,40 @@
-package ru.naumen.sd40.log.parser.Parsers;
+package ru.naumen.sd40.log.parser.TimeParsers;
 
-public class GCParser {
+import org.springframework.stereotype.Service;
+import ru.naumen.sd40.log.parser.Interfaces.ITimeParser;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+@Service
+public class GCTimeParser implements ITimeParser{
+
+    private static final Pattern PATTERN = Pattern
+            .compile("^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}.\\d{3}\\+\\d{4}).*");
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+            new Locale("ru", "RU"));
+
+    public long parseTime(String line) throws ParseException {
+        Matcher matcher = PATTERN.matcher(line);
+
+        if (matcher.find())
+        {
+            Date parse = DATE_FORMAT.parse(matcher.group(1));
+            return parse.getTime();
+        }
+        return 0L;
+    }
+
+    public void setTimeZone(String timeZone){
+        if(timeZone == null)
+            timeZone = "GMT";
+        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone(timeZone));
+    }
 }
